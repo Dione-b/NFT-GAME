a251d8d2b22be9712828e29287034cce3f243797
dione-b <dionenassau321@gmail.com>
2022-09-06 17:40:25 -0300

    v1.1.1

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,17 @@
 require("@nomiclabs/hardhat-waffle");
 
+
 module.exports = {
   solidity: "0.8.4",
   networks: {
     goerli: {
-      url: "https://eth-goerli.g.alchemy.com/v2/iweBLF_SgA4uLPseBOSiwXXF-_NjmEnF",
-      accounts: ["4b7b5323a9d9bcbde6f5f0885021e0ca85dcd4e119b80fe43d3fdc82d146b24f"],
+      url: process.env.STAGING_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    mainnet: {
+      chainId: 1,
+      url: process.env.PROD_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY],
     },
   },
 };
